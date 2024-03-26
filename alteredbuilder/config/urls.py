650e132d832550bee104e02fd96504518ecb05cf
Ajordat <alexjortri@gmail.com>
2024-03-26 16:59:52 +0100

    Added 500 error page

--- alteredbuilder/config/urls.py
+++ alteredbuilder/config/urls.py
@@ -23,6 +23,7 @@ from django.views.generic import RedirectView, TemplateView
 # Error files definitions
 handler403 = TemplateView.as_view(template_name="errors/403.html")
 handler404 = TemplateView.as_view(template_name="errors/404.html")
+handler500 = TemplateView.as_view(template_name="errors/500.html")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
