880e8152ffece5a77b562f87db6cdaada536249a
Ajordat <alexjortri@gmail.com>
2024-04-15 13:00:08 +0200

    Created template snippet for card type tables

--- alteredbuilder/decks/templatetags/deck_styles.py
+++ alteredbuilder/decks/templatetags/deck_styles.py
@@ -22,3 +22,11 @@ def get_row_color_from_rarity(rarity: str) -> str:
             return "table-danger"
         case _:
             return ""
+
+@register.simple_tag
+def get_main_cost(card):
+    return getattr(card, card.type).main_cost
+
+@register.simple_tag
+def get_recall_cost(card):
+    return getattr(card, card.type).recall_cost
