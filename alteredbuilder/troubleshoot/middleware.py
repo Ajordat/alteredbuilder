43b26c47945dda065bbf37276b56163d5c526238
Ajordat <alexjortri@gmail.com>
2024-06-12 16:37:03 +0200

    Remove troubleshooting logs

--- alteredbuilder/troubleshoot/middleware.py
+++ alteredbuilder/troubleshoot/middleware.py
@@ -9,15 +9,6 @@ class TroubleshootingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest):
-        if request.path in [reverse("troubleshoot:session")]:
-            print(f"{request.path=}")
-            print(f"{request.user.username=}")
-            print(f"{request.session.session_key=}")
-            print(f"{request.COOKIES}")
-            try:
-                print(f"{request.session["_auth_user_id"]=}")
-            except KeyError:
-                print("No key `_auth_user_id`")
 
         response = self.get_response(request)
 
@@ -25,7 +16,6 @@ class TroubleshootingMiddleware:
             url_location = response.headers["Location"]
             url = []
             for url_fragment in url_location.split("&"):
-                print(url_fragment)
                 if url_fragment.startswith("redirect_uri"):
                     url += [urlencode({"redirect_uri": f"https://{request.headers["X-Forwarded-Host"]}/accounts/discord/login/callback/"})]
                 else:
