2e471ca05750231f0259c5ee721fc10ed8685f69
Ajordat <alexjortri@gmail.com>
2024-06-27 20:41:50 +0200

    Add robots.txt file

--- alteredbuilder/config/urls.py
+++ alteredbuilder/config/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
         RedirectView.as_view(url=reverse_lazy("deck-list"), permanent=True),
         name="index",
     ),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 urlpatterns += i18n_patterns(
