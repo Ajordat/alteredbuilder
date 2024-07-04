77f39e6943187ccc0bc02f464331564559a636c6
Ajordat <alexjortri@gmail.com>
2024-07-04 18:28:46 +0200

    Add markdown instructions

--- alteredbuilder/decks/templatetags/markdown_extras.py
+++ alteredbuilder/decks/templatetags/markdown_extras.py
@@ -6,6 +6,7 @@ from django.template.defaultfilters import stringfilter
 import markdown as md
 from markdown.extensions import Extension
 from markdown.inlinepatterns import InlineProcessor
+from markdown.treeprocessors import Treeprocessor
 
 from decks.models import Card
 
@@ -38,13 +39,33 @@ class InlineCardReferenceProcessor(InlineProcessor):
         return element, m.start(0), m.end(0)
 
 
-class AlteredCardsExtension(Extension):
+class AlteredExtension(Extension):
     def extendMarkdown(self, md: md.Markdown) -> None:
         md.inlinePatterns.register(
             InlineCardReferenceProcessor(REFERENCE_RE, md),
             "inlinecardreferenceprocessor",
             30,
         )
+
+
+class InlineImageProcessor(Treeprocessor):
+    def run(self, root: Element) -> Element | None:
+        for element in root.iter('img'):
+            attrib = element.attrib
+            tail = element.tail
+            element.clear()
+            element.tag = 'a'
+            element.attrib['href'] = attrib.pop('src')
+            element.text = attrib.pop('alt')
+            element.tail = tail
+            # element.attrib |= attrib
+            for k, v in attrib.items():
+                element.set(k, v)
+
+
+class ImageExtension(Extension):
+    def extendMarkdown(self, md: md.Markdown) -> None:
+        md.treeprocessors.register(InlineImageProcessor(), 'inlineimageprocessor', 20)
 
 
 register = template.Library()
@@ -61,4 +82,4 @@ def markdown(value: str) -> str:
     Returns:
         str: HTML code version of the received string.
     """
-    return md.markdown(value, extensions=["markdown_mark", AlteredCardsExtension()])
+    return md.markdown(value, extensions=["markdown_mark", AlteredExtension(), ImageExtension()])
