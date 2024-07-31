608e07cd0ddfd9fada5cf4f4de4f36a680dbc72b
Ajordat <alexjortri@gmail.com>
2024-07-31 18:55:31 +0200

    Convert stats to int

---
+++ alteredbuilder/decks/migrations/0052_convert_stats_to_int.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+
+
+def init_models(apps):
+    global Card
+    Card = apps.get_model("decks", "Card")
+
+
+def fix_stats_values(apps, schema_editor):
+    init_models(apps)
+
+    updated_cards = []
+    for card in Card.objects.all():
+        for field, value in card.stats.items():
+            # if "#" in str(value):
+            card.stats[field] = int(value)
+            updated_cards.append(card)
+    Card.objects.bulk_update(updated_cards, ["stats"])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("decks", "0051_fix_hashtag_on_stats_field"),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_stats_values),
+    ]
