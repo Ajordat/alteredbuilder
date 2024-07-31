4e0219015abbda802a713dd6432d64e8a68bc60c
Ajordat <alexjortri@gmail.com>
2024-07-31 18:45:31 +0200

    Fix stats field on card

---
+++ alteredbuilder/decks/migrations/0051_fix_hashtag_on_stats_field.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+
+
+def init_models(apps):
+    global Card
+    Card = apps.get_model("decks", "Card")
+
+
+def fix_stats_values(apps, schema_editor):
+    init_models(apps)
+
+    updated_cards = []
+    for card in Card.objects.all():
+        for field, value in card.stats.items():
+            if "#" in str(value):
+                card.stats[field] = value.strip("#")
+                updated_cards.append(card)
+    Card.objects.bulk_update(updated_cards, ["stats"])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("decks", "0050_rename_echo_effect_temp_card_echo_effect_and_more"),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_stats_values),
+    ]
