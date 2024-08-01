0d2aa9df9634c9ede8d2cc63f463e4fc2ae17dd4
Ajordat <alexjortri@gmail.com>
2024-08-01 02:03:46 +0200

    Fix hero set

---
+++ alteredbuilder/decks/migrations/0053_migrate_heroes_to_promo_set.py
@@ -0,0 +1,28 @@
+
+from django.db import migrations
+
+
+def init_models(apps):
+    global Card, Set
+    Card = apps.get_model("decks", "Card")
+    Set = apps.get_model("decks", "Set")
+
+
+def link_cards_to_sets(apps, schema_editor):
+
+    card_set = Set.objects.get(code="COREP")
+
+    Card.objects.filter(reference__contains=card_set.reference_code).update(
+        set=card_set
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("decks", "0052_convert_stats_to_int"),
+    ]
+
+    operations = [
+        migrations.RunPython(link_cards_to_sets),
+    ]
