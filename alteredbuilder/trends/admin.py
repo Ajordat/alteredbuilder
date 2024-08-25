8fd340e9e74f57f832214d04fd2ea36bf036eeb1
Ajordat <alexjortri@gmail.com>
2024-08-25 19:39:20 +0200

    Add debug actions to trends

--- alteredbuilder/trends/admin.py
+++ alteredbuilder/trends/admin.py
@@ -1,21 +1,44 @@
-from django.contrib import admin
+from collections import OrderedDict
+from datetime import timedelta
+from typing import Any
 
-from .models import CardTrend, FactionTrend, HeroTrend
+from django.conf import settings
+from django.contrib import admin
+from django.http import HttpRequest
+from django.utils.timezone import localdate
+
+from trends.models import CardTrend, FactionTrend, HeroTrend
+
+
+class TrendUtilities:
+    actions = ["move_trend"]
+
+    def get_actions(self, request: HttpRequest) -> OrderedDict[Any, Any]:
+        actions = super().get_actions(request)
+        if not settings.DEBUG:
+            del actions["move_trend"]
+        return actions
+
+    @admin.action(description="Move trend to current timestamps")
+    def move_trend(self, request, queryset):
+        yesterday = localdate() - timedelta(days=1)
+        updated = queryset.update(date=yesterday)
+        self.message_user(request, f"{updated} trends were moved to yesterday.")
 
 
 @admin.register(FactionTrend)
-class FactionTrendAdmin(admin.ModelAdmin):
+class FactionTrendAdmin(TrendUtilities, admin.ModelAdmin):
     list_display = ["date", "count", "faction"]
     list_filter = ["faction"]
 
 
 @admin.register(HeroTrend)
-class HeroTrendAdmin(admin.ModelAdmin):
+class HeroTrendAdmin(TrendUtilities, admin.ModelAdmin):
     list_display = ["date", "count", "hero"]
 
 
 @admin.register(CardTrend)
-class CardTrendAdmin(admin.ModelAdmin):
+class CardTrendAdmin(TrendUtilities, admin.ModelAdmin):
     list_display = ["date", "ranking", "card", "faction", "hero"]
     search_fields = ["hero__name_en", "card__name_en"]
     list_filter = ["faction", "ranking"]
