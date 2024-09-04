3959018ea04b1aff6729163d899168ab502d018e
Ajordat <alexjortri@gmail.com>
2024-09-04 19:52:18 +0200

    Disable playtest

--- alteredbuilder/config/context_processors.py
+++ alteredbuilder/config/context_processors.py
@@ -1,7 +1,11 @@
 from django.conf import settings
+from django.utils.timezone import datetime
 import environ
 
 
 def add_version(request):
     env = environ.Env(COMMIT_ID=(str, "XXXXXXX"))
     return {"version": settings.VERSION, "build": env("COMMIT_ID")}
+
+def add_release_date(request):
+    return {"release_date": datetime(2024, 9, 13)}
