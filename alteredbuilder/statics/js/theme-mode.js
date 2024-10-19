7c712978b657b088cd50b3e0ce5953380c8b5490
Ajordat <alexjortri@gmail.com>
2024-10-19 23:11:34 +0200

    Added style to release accordion

--- alteredbuilder/statics/js/theme-mode.js
+++ alteredbuilder/statics/js/theme-mode.js
@@ -30,15 +30,6 @@
       if (theme === 'auto') {
         theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
       }
-      let activeTabElement = document.getElementsByClassName("current-nav-tab")[0];
-      if (!activeTabElement) return;
-      if (theme === 'dark') {
-        // activeTabElement.classList.add("bg-dark");
-        // activeTabElement.classList.remove("bg-white");
-      } else {
-        // activeTabElement.classList.add("bg-white");
-        // activeTabElement.classList.remove("bg-dark");
-      }
     }
   
     setTheme(getPreferredTheme())
