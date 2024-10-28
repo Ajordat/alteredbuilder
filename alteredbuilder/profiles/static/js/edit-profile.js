e72a00536337023eccadda3bada8614f91655bf8
Ajordat <alexjortri@gmail.com>
2024-10-28 19:26:24 +0100

    Correctly prefix avatar img's static url when editing a profile

--- alteredbuilder/profiles/static/js/edit-profile.js
+++ alteredbuilder/profiles/static/js/edit-profile.js
@@ -3,7 +3,7 @@ document.querySelectorAll('.profile-pic-choice').forEach(function(pic) {
     pic.addEventListener('click', function() {
         // Get the clicked picture's value and image URL
         const selectedPicName = this.getAttribute('data-pic-name');
-        const selectedPicUrl = `/static/img/avatars/${selectedPicName}`;
+        const selectedPicUrl = `${avatarBaseUrl}${selectedPicName}`;
 
         // Update the displayed image and name
         document.getElementById('selected-profile-pic').src = selectedPicUrl;
